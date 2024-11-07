9992b3a8cf913df95ab51f0c7332676fec8709f6
SagarSharma656 <sagar.237052@gmail.com>
2024-11-07 14:58:51 +0530

    add login for normal usr

--- src/component/Home/UserLogin.jsx
+++ src/component/Home/UserLogin.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from '../../styles/userLogin.module.css'
+import logo from '../../assets/logo.png'
+import { BiLeftArrowAlt } from 'react-icons/bi'
+
 
 const UserLogin = () => {
-  return (
-    <div>UserLogin</div>
-  )
+
+    return (
+        <div className={styles.wraper}>
+            <div className={styles.container}>
+
+
+                <div className={styles.imgContainer}>
+                    <div onClick={() => { }} className={styles.backBtnContainer}>
+                        <BiLeftArrowAlt className={styles.backBtn} />
+                    </div>
+                    <img src={logo} />
+                </div>
+                <h3>India's last minute app</h3>
+                <p>Log in or Sign up</p>
+
+                <div>
+                    <input type="text" />
+                </div>
+
+                <button>Continue</button>
+
+            </div>
+        </div >
+    )
 }
 
 export default UserLogin
